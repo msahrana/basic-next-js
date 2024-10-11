e4dc7f22fbcb2cb2759b3428f380863e1489bd96
msahrana <msahrana@gmail.com>
2024-10-11 09:51:50 +0600

    banner part update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import LogoImg from "../../public/logo.png";
+import LogoImg from "../../public/logo.jpg";
 import Image from "next/image";
 
 const Banner = () => {
   return (
     <div>
-      <Image src={LogoImg} alt="logo-image" width={1800} height={400} />
+      <Image src={LogoImg} alt="logo-image" width={1920} height={600} />
     </div>
   );
 };
